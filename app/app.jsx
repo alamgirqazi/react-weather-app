3df8af466b99c717e9199388b61f3fadc7b29fd0
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-01-30 16:57:07 +0500

    added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,11 @@ var Weather = require("Weather");
 var About = require('About');
 var Examples = require('Examples');
 
+//load foundation
+
+require('style!css!foundation-sites/dist/foundation.min.css')
+
+$(document).foundation();
 
 ReactDOM.render(
   //Props passed
