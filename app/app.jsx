c9ccb141e3a45a1586b645a81df4bca260ebfed9
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-02-09 16:51:23 +0500

    added scss and webpack loaders

--- app/app.jsx
+++ app/app.jsx
@@ -13,7 +13,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 
 // app.css
 
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 $(document).foundation();
 
