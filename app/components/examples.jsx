f92264e66993c53f81d3185ac569d730c08da824
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-02-10 09:26:45 +0500

    fixed modal issue

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -6,7 +6,7 @@ var Examples = (props) =>
 return (
 <div>
   
-  <h1 className="text-center page-title">Example component!</h1>
+  <h1 className="text-center page-title">Example</h1>
 <p>Here are a few example locations to try out: </p>
 
 <ol>
